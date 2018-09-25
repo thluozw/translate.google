c9bed8b04a4161d874cb9987ca5703120492d51a
thluozw <thluozw@163.com>
2018-09-25 20:52:30 +0800

    Initial commit

---
+++ NetTranslate/Translate.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Web;
+using MSScriptControl;
+using Newtonsoft.Json;
+
+namespace Translate
+{
+    public class TranslateClass
+    {
+
+        public double timeConsuming { get; set; }
+
+        /// <summary>
+        /// 谷歌翻译
+        /// </summary>
+        /// <param name="text">待翻译文本</param>
+        /// <param name="fromLanguage">自动检测：auto</param>
+        /// <param name="toLanguage">中文：zh-CN，英文：en</param>
+        /// <returns>翻译后文本</returns>
+        public string GoogleTranslate(string text, string fromLanguage, string toLanguage)
+        {
+            CookieContainer cc = new CookieContainer();
+            int aa = Environment.TickCount;
+            string GoogleTransBaseUrl = "https://translate.google.cn/";
+
+            var BaseResultHtml = GetResultHtml(GoogleTransBaseUrl, cc, "");
+
+            Regex re = new Regex(@"(?<=TKK=)(.*?)(?=;)(?<=\')");
+
+            var TKKStr = re.Match(BaseResultHtml).ToString() /*+ ")"*/;//在返回的HTML中正则匹配TKK的JS代码
+
+            var TKK = new Regex(@"(?<=\')[^}]*(?=\')").Match(TKKStr).ToString(); //ExecuteScript(TKKStr, TKKStr);//执行TKK代码，得到TKK值
+
+            var GetTkkJS = File.ReadAllText("./gettk");
+
+            var tk = ExecuteScript("tk(\"" + text + "\",\"" + TKK + "\")", GetTkkJS);
+
+            string googleTransUrl = "https://translate.google.cn/translate_a/single?client=t&sl=" + fromLanguage + "&tl=" + toLanguage + "&hl=en&dt=at&dt=bd&dt=ex&dt=ld&dt=md&dt=qca&dt=rw&dt=rm&dt=ss&dt=t&ie=UTF-8&oe=UTF-8&otf=1&ssel=0&tsel=0&kc=1&tk=" + tk + "&q=" + HttpUtility.UrlEncode(text);
+
+            var ResultHtml = GetResultHtml(googleTransUrl, cc, "https://translate.google.cn/");
+
+            dynamic TempResult = JsonConvert.DeserializeObject(ResultHtml);
+
+            string ResultText = Convert.ToString(TempResult[0][0][0]);
+            timeConsuming = Environment.TickCount - aa;
+            return ResultText;
+        }
+
+        public string GetResultHtml(string url, CookieContainer cc, string refer)
+        {
+            var html = "";
+
+            var webRequest = WebRequest.Create(url) as HttpWebRequest;
+
+            webRequest.Method = "GET";
+
+            webRequest.CookieContainer = new CookieContainer();
+
+            //webRequest.Referer = referer;
+
+            webRequest.Timeout = 20000;
+
+            webRequest.Headers.Add("X-Requested-With:XMLHttpRequest");
+
+            webRequest.Accept = "*/*";
+            //webRequest.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+
+            webRequest.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36";
+
+            using (var webResponse = (HttpWebResponse)webRequest.GetResponse())
+            {
+                using (var reader = new StreamReader(webResponse.GetResponseStream(), Encoding.UTF8))
+                {
+
+                    html = reader.ReadToEnd();
+                    reader.Close();
+                    webResponse.Close();
+                }
+            }
+            return html;
+        }
+
+        /// <summary>
+        /// 执行JS
+        /// </summary>
+        /// <param name="sExpression">参数体</param>
+        /// <param name="sCode">JavaScript代码的字符串</param>
+        /// <returns></returns>
+        private string ExecuteScript(string sExpression, string sCode)
+        {
+            ScriptControl scriptControl = new ScriptControl();
+            scriptControl.UseSafeSubset = true;
+            scriptControl.Language = "JScript";
+            scriptControl.AddCode(sCode);
+            try
+            {
+                string str = scriptControl.Eval(sExpression).ToString();
+                return str;
+            }
+            catch (Exception ex)
+            {
+                string str = ex.Message;
+            }
+            return null;
+        }
+        
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Translate;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TranslateClass translate = new TranslateClass();
+            Console.WriteLine("Translate.Google.cn");
+            string cn = Console.ReadLine();
+            while(cn != "Q")
+            {
+                string str = translate.GoogleTranslate(cn, "zh-CN", "en");
+                Console.WriteLine(str);
+                cn = Console.ReadLine();
+            }
+        }
+    }
+}
